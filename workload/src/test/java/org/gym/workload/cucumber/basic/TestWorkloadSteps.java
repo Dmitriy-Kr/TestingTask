2b34208d58a492694c626773460648d727d44230
Dmitriy Kraskovskyy <kraskovskyydmitriy@gmail.com>
2025-02-01 17:09:29 +0200

    Added scenarios for testing workload microservice

--- workload/src/test/java/org/gym/workload/cucumber/basic/TestWorkloadSteps.java
+++ workload/src/test/java/org/gym/workload/cucumber/basic/TestWorkloadSteps.java
@@ -32,36 +32,162 @@ public class TestWorkloadSteps {
 
     private WorkloadMessage message;
 
-    @Given("Message with trainer workload: year, month, day, duration in minutes")
-    public void message_with_trainer_workload_year_month_day_duration_in_minutes() {
+    private String request;
 
-        message = new WorkloadMessage();
-        message.setTrainerUsername("Carina.Cox");
-        message.setTrainerFirstName("Carina");
-        message.setTrainerLastName("Cox");
-        message.setTrainingDuration(295);
-        message.setTrainingDate(Date.valueOf("2025-12-01"));
-        message.setActionType(WorkloadMessage.ActionType.ADD);
+    private String response;
+
+    @When("Microservice receive the request: username - {string}, year - {int}, month - {int}")
+    public void microserviceReceiveTheRequestUsernameKathleenCarrYearMonth(String username, int year, int month) {
+
+        request = "/workload?username=%s&year=%d&month=%d".formatted(username, year, month);
+
     }
 
-    @When("Microservice receive the message")
-    public void microservice_receive_the_message() {
+    @Then("The workload service sends response: {string}")
+    public void theWorkloadSendResponseWorkloadForKathleenCarrInMARCHIsHrsMin(String expectedResponse) {
+
+        response = restTemplate.getForObject(request, String.class);
+
+        assertThat(response).isEqualTo(expectedResponse);
+
+    }
+
+    @Given("Message with trainer workload: {string}, {string}, {string}, {string}, {int}, {string}")
+    public void messageWithTrainerWorkload(String username, String firstname, String lastname, String date, int duration, String action) {
+
+        message = new WorkloadMessage();
+        message.setTrainerUsername(username);
+        message.setTrainerFirstName(firstname);
+        message.setTrainerLastName(lastname);
+        message.setTrainingDate(Date.valueOf(date));
+        message.setTrainingDuration(duration);
+        message.setActionType(WorkloadMessage.ActionType.valueOf(action));
+
+    }
+
+    @When("Microservice receives the message")
+    public void microservice_receives_the_message() {
 
         jmsTemplate.convertAndSend("workload-queue", message);
 
     }
 
-    @Then("The new trainer \\(with workload) added to database")
-    public void the_new_trainer_with_workload_added_to_database() {
+    @Then("Send request to workload service and get {string}")
+    public void the_new_trainer_with_workload_added_to_database(String expectedResponse) {
 
-        String response = restTemplate.getForObject("/workload?username=%s&year=%d&month=%d"
-                .formatted(message.getTrainerUsername(),
-                        message.getTrainingDate().toLocalDate().getYear(),
-                        message.getTrainingDate().toLocalDate().getMonthValue()),
+        delay();
+
+        response = restTemplate.getForObject("/workload?username=%s&year=%d&month=%d"
+                        .formatted(message.getTrainerUsername(),
+                                message.getTrainingDate().toLocalDate().getYear(),
+                                message.getTrainingDate().toLocalDate().getMonthValue()),
                 String.class);
 
-        assertThat(response).isEqualTo("Workload for Carina.Cox in DECEMBER 2025 is 4 hrs 55 min");
+        assertThat(response).isEqualTo(expectedResponse);
 
+    }
+
+    @Given("Message with workload of existing in db trainer")
+    public void messageWithWorkloadOfExistingInDbTrainerYearMonthDayDurationInMinutes() {
+
+        message = new WorkloadMessage();
+        message.setTrainerUsername("Coleman.Yates");
+        message.setTrainerFirstName("Coleman");
+        message.setTrainerLastName("Yates");
+        message.setTrainingDuration(165); //2 hrs 45 min
+        message.setTrainingDate(Date.valueOf("2025-10-01"));
+        message.setActionType(WorkloadMessage.ActionType.ADD);
+
+    }
+
+    @When("Microservice receive the message with workload of existing in db trainer")
+    public void microserviceReceiveTheMessageWithWorkloadOfExistingInDbTrainer() {
+
+        jmsTemplate.convertAndSend("workload-queue", message);
+
+    }
+
+
+    @Then("The workload was added to database")
+    public void theWorkloadAddedToDatabase() {
+
+        delay();
+
+        response = restTemplate.getForObject("/workload?username=%s&year=%d&month=%d"
+                        .formatted(message.getTrainerUsername(),
+                                message.getTrainingDate().toLocalDate().getYear(),
+                                message.getTrainingDate().toLocalDate().getMonthValue()),
+                String.class);
+
+        assertThat(response).isEqualTo("Workload for Coleman.Yates in OCTOBER 2025 is 2 hrs 45 min");
+
+    }
+
+    @Given("Not valid message")
+    public void notValidMessage() {
+        message = new WorkloadMessage();
+        message.setTrainerUsername("Not valid");
+    }
+
+    @When("Microservice receive the message and put it in Dead Letter Queue")
+    public void microserviceReceiveTheMessageAndPutItInDeadLetterQueue() {
+
+        jmsTemplate.convertAndSend("workload-queue", message);
+
+    }
+
+    @Then("Dead Letter Queue contains the message")
+    public void deadLetterQueueContainsTheMessage() {
+
+        WorkloadMessage receivedMessage = (WorkloadMessage) jmsTemplate.receiveAndConvert("ActiveMQ.DLQ");
+
+        assertThat(receivedMessage).isNotNull();
+
+        assertThat(receivedMessage.getTrainerUsername()).isEqualTo("Not valid");
+
+    }
+
+    @Given("Message with workload to delete: {string}, {string}, {string}, {string}, {int}, {string}")
+    public void messageWithWorkloadToDeleteDuration(String username, String firstname, String lastname, String date, int duration, String action) {
+
+        message = new WorkloadMessage();
+        message.setTrainerUsername(username);
+        message.setTrainerFirstName(firstname);
+        message.setTrainerLastName(lastname);
+        message.setTrainingDate(Date.valueOf(date));
+        message.setTrainingDuration(duration);
+        message.setActionType(WorkloadMessage.ActionType.valueOf(action));
+
+    }
+
+    @When("Microservice receive the message with workload to delete")
+    public void microserviceReceiveTheMessageWithWorkloadToDelete() {
+
+        jmsTemplate.convertAndSend("workload-queue", message);
+
+    }
+
+    @Then("Send request to workload service to check and get {string}")
+    public void sendRequestToWorkloadServiceToCheckAndGet(String expectedResponse) {
+
+        delay();
+
+        response = restTemplate.getForObject("/workload?username=%s&year=%d&month=%d"
+                        .formatted(message.getTrainerUsername(),
+                                message.getTrainingDate().toLocalDate().getYear(),
+                                message.getTrainingDate().toLocalDate().getMonthValue()),
+                String.class);
+
+        assertThat(response).isEqualTo(expectedResponse);
+
+    }
+
+    private void delay() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
--- workload/src/test/resources/cucumber/testworkload.feature
+++ workload/src/test/resources/cucumber/testworkload.feature
@@ -1,12 +1,35 @@
 Feature: Test workload
   Test workload microservice functionality
 
-  Scenario: Add trainer workload to database when trainer isn't in database
-    Given Message with trainer workload: year, month, day, duration in minutes
-    When Microservice receive the message
-    Then The new trainer (with workload) added to database
+  Scenario: Get existing trainer workload from database
+    When Microservice receive the request: username - "Kathleen.Carr", year - 2023, month - 3
+    Then The workload service sends response: "Workload for Kathleen.Carr in MARCH 2023 is 0 hrs 20 min"
 
-#  Scenario: Add trainer workload to database when trainer is in database
-#    Given Message with trainer workload: year, month, day, duration in minutes
-#    When Microservice receive the message
-#    Then The workload) added to database
+  Scenario Outline: Add trainer workload to database when trainer isn't exist in database
+    Given Message with trainer workload: "<username>", "<firstname>", "<lastname>", "<date>", <duration>, "<action>"
+    When Microservice receives the message
+    Then Send request to workload service and get "<response>"
+
+    Examples:
+      | username    | firstname | lastname | date       | duration | action | response                                                 |
+      | Carina.Cox  | Carina    | Cox      | 2025-12-01 | 295      | ADD    | Workload for Carina.Cox in DECEMBER 2025 is 4 hrs 55 min |
+
+  Scenario: Add trainer workload to database when trainer is in database
+    Given Message with workload of existing in db trainer
+    When Microservice receive the message with workload of existing in db trainer
+    Then The workload was added to database
+
+  Scenario: Send not valid request to service
+    Given Not valid message
+    When Microservice receive the message and put it in Dead Letter Queue
+    Then Dead Letter Queue contains the message
+
+  Scenario Outline: Delete existing trainer workload
+    Given Message with workload to delete: "<username>", "<firstname>", "<lastname>", "<date>", <duration>, "<action>"
+    When Microservice receive the message with workload to delete
+    Then Send request to workload service to check and get "<response>"
+
+    Examples:
+      | username       | firstname | lastname | date       | duration | action | response                                                |
+      | Ward.Mejia     | Ward      | Mejia    | 2024-01-01 | 20       | DELETE | Workload for Ward.Mejia in JANUARY 2024 is 0 hrs 20 min |
+      | Kathleen.Carr  | Kathleen  | Carr     | 2023-05-01 | 15       | DELETE | Workload for Kathleen.Carr in MAY 2023 is 0 hrs 30 min  |
